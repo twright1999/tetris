f7504580739aa91cc5793d17eb119db708acf591
Thomas <thomasjameswright@hotmail.co.uk>
2019-12-13 01:51:57 +0000

    basic game loop and .gitignore

---
+++ game.py
@@ -0,0 +1,40 @@
+"""
+game.py
+
+contains main game loop
+"""
+
+import pygame as pg
+
+
+class Game:
+
+	def __init__(self):
+		self.caption = "Tetris"
+		self.screen = pg.display.set_mode((800,400))
+		self.clock = pg.time.Clock()
+		self.done = False
+
+	def main(self):
+		pg.init()
+
+		while not self.done:
+			self.input()
+			self.update()
+			self.draw()
+
+			self.clock.tick(60)
+
+		pg.quit()
+
+	def input(self):
+		for event in pg.event.get():
+			if event.type == pg.QUIT:
+				self.done = True
+
+	def update(self):
+		temp = "temp"
+
+	def draw(self):
+		self.screen.fill((0,0,0))
+		pg.display.flip()
---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+main.py
+
+Contains creates a game object and runs it
+"""
+
+import game
+
+def main():
+	current_game = game.Game()
+
+	current_game.main()
+
+if __name__=="__main__":
+	main()
